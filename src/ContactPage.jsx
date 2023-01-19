f98f2b2a59c073da1e66a1c4d2598d660f1fc08a
nelly476 <nellybulatova12@gmail.com>
2023-01-19 12:35:56 +0300

    add form state

--- src/ContactPage.jsx
+++ src/ContactPage.jsx
@@ -2,31 +2,84 @@ import image from "./assets/bg-image.jpg";
 import { useState } from "react";
 
 export default function Products() {
+  const [formData, setFormData] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+    number: "",
+    isAccepted: false,
+  });
+
+  function handleChange(e) {
+    setFormData((prev) => {
+      const { name, checked, type, value } = e.target;
+
+      return {
+        ...prev,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // console.log(Object.values(formData));
+    if (Object.values(formData).every((item) => item)) {
+      console.log(formData);
+    } else {
+      console.log("not all");
+    }
+  }
+
   return (
     <div className="main-container">
       <span>
         <header>
           <h1>Contact Us</h1>
         </header>
-        <form>
+        <form onSubmit={handleSubmit}>
           <p>
-            <label htmlFor="fname">First name</label>
-            <input type="text" name="fname" id="fname" />
+            <label htmlFor="fName">First name</label>
+            <input
+              type="text"
+              name="fName"
+              id="fName"
+              onChange={handleChange}
+              value={formData.fName}
+            />
           </p>
 
           <p>
-            <label htmlFor="lname">Last name</label>
-            <input type="text" name="lname" id="lname" />
+            <label htmlFor="lName">Last name</label>
+            <input
+              type="text"
+              name="lName"
+              id="lName"
+              onChange={handleChange}
+              value={formData.lName}
+            />
           </p>
 
           <p>
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              onChange={handleChange}
+              value={formData.email}
+            />
           </p>
 
           <p>
             <label htmlFor="number">Phone number</label>
-            <input type="tel" name="number" id="number" />
+            <input
+              type="tel"
+              name="number"
+              id="number"
+              onChange={handleChange}
+              value={formData.number}
+            />
           </p>
 
           <p className="agreement--section">
@@ -35,13 +88,15 @@ export default function Products() {
             </label>
             <input
               type="checkbox"
-              name="agreement"
+              name="isAccepted"
               id="agreement"
               className="agreement--checkbox"
+              onChange={handleChange}
+              checked={formData.isAccepted}
             />
           </p>
 
-          <input type="submit" class="btn" value="Submit"></input>
+          <input type="submit" className="btn" value="Submit"></input>
         </form>
       </span>
       <img src={image} alt="" />

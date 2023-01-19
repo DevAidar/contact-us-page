9c30698cda0425c89c74bdd476ca4b76358466e7
nelly476 <nellybulatova12@gmail.com>
2023-01-19 23:19:19 +0300

    add media query

--- src/ContactPage.jsx
+++ src/ContactPage.jsx
@@ -34,68 +34,63 @@ export default function Products() {
   }
 
   return (
-    <div className="main-container">
-      <span>
-        <header>
-          <h1>Contact Us</h1>
-        </header>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="fName">First name</label>
-          <input
-            type="text"
-            name="fName"
-            onChange={handleChange}
-            value={formData.fName}
-            className={submitClicked && !formData.fName ? "highlight" : ""}
-          />
+    <div className="main--container">
+      <header>
+        <h1>Contact Us</h1>
+      </header>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="fName">First name</label>
+        <input
+          type="text"
+          name="fName"
+          onChange={handleChange}
+          value={formData.fName}
+          className={submitClicked && !formData.fName ? "highlight" : ""}
+        />
 
-          <label htmlFor="lName">Last name</label>
-          <input
-            type="text"
-            name="lName"
-            onChange={handleChange}
-            value={formData.lName}
-            className={submitClicked && !formData.lName ? "highlight" : ""}
-          />
+        <label htmlFor="lName">Last name</label>
+        <input
+          type="text"
+          name="lName"
+          onChange={handleChange}
+          value={formData.lName}
+          className={submitClicked && !formData.lName ? "highlight" : ""}
+        />
 
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            value={formData.email}
-            className={submitClicked && !formData.email ? "highlight" : ""}
-          />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={formData.email}
+          className={submitClicked && !formData.email ? "highlight" : ""}
+        />
 
-          <label htmlFor="number">Phone number</label>
-          <input
-            type="number"
-            name="number"
-            onChange={handleChange}
-            value={formData.number}
-            className={submitClicked && !formData.number ? "highlight" : ""}
-          />
+        <label htmlFor="number">Phone number</label>
+        <input
+          type="number"
+          name="number"
+          onChange={handleChange}
+          value={formData.number}
+          className={submitClicked && !formData.number ? "highlight" : ""}
+        />
 
-          <div className="agreement--section">
-            <label htmlFor="agreement">
-              I agree to the terms and conditions
-            </label>
-            <input
-              type="checkbox"
-              name="isAccepted"
-              className={
-                submitClicked && !formData.isAccepted ? "highlight" : ""
-              }
-              onChange={handleChange}
-              checked={formData.isAccepted}
-            />
-          </div>
-          <div className="button--container">
-            <button>Submit</button>
-          </div>
-        </form>
-      </span>
-      <img src={image} alt="" />
+        <div className="agreement--section">
+          <label htmlFor="agreement">I agree to the terms and conditions</label>
+          <input
+            type="checkbox"
+            name="isAccepted"
+            className={submitClicked && !formData.isAccepted ? "highlight" : ""}
+            onChange={handleChange}
+            checked={formData.isAccepted}
+          />
+        </div>
+        <div className="button--container">
+          <button>Submit</button>
+        </div>
+      </form>
+
+      <img src={image} alt="image of mountains" />
     </div>
   );
 }
